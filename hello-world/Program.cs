74c6996cf603da526d5ed1ea04a847c1b2264857
Jeriko <jcarrico1985@live.com>
2018-03-02 15:49:21 -0600

    add entire project. probably shouldnt do it but oh well, live and learn.

---
+++ hello-world/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace helloworld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 10.95f;
+            char letter = 'A';
+            string name = "Jon";
+            bool isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(letter);
+            Console.WriteLine(name);
+            Console.WriteLine(isWorking);
+        }
+    }
+}

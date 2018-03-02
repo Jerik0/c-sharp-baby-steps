32d96a5232b1d036dca22a042998c72556a208be
Jeriko <jcarrico1985@live.com>
2018-03-02 16:34:12 -0600

    playing with type conversion

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -6,8 +6,20 @@ namespace helloworld
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            byte b = 1;
+            int i = b; //no data loss because int *expands* bytes by 3 for a total of 4 bytes.
+            //IMPLICIT data conversion.
+
+            int j = 1;
+            byte b2 = (byte) j; //no data loss because j is smaller than 255. Anything larger would result in loss.
+            //EXPLICIT data conversion using casting.
+
+            int k = 1000;
+            byte b3 = (byte)k; //loss because k is larger than 255.
+
+            Console.WriteLine("{0} {1}", b, i);
+            Console.WriteLine(b2);
+            Console.WriteLine(b3);
         }
     }
 }
